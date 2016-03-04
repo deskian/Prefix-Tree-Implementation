6e37875ab2f23b3698a30b32c136d40294632f2a
Long Nguyen <caycham1@gmail.com>
2016-03-04 01:17:46 -0600

    Ran yo test to generate test suite

---
+++ spec/prefix_tree.js
@@ -0,0 +1,20 @@
+/* global prefix_tree, describe, it, expect, should */
+
+describe('prefix_tree()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(prefix_tree).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
